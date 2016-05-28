3988e2c9c639917db0fad272ca2ccdcb17e60025
Caleb Jones <code@calebjones.net>
2016-05-27 22:57:45 -0400

    Fix the tests again

--- test.c
+++ test.c
@@ -1,21 +1,15 @@
 #include "minunit.h"
 #include <stdbool.h>
 
-char *passing_test(void) {
+char *trivial_test(void) {
   mu_assert(true, "This test should pass");
-  return NULL;
-}
-
-char *failing_test(void) {
-  mu_assert(false, "This test probably just failed");
   return NULL;
 }
 
 char *test_suite(void) {
   mu_suite_start();
 
-  mu_run_test(passing_test);
-  mu_run_test(failing_test);
+  mu_run_test(trivial_test);
 
   return NULL;
 }
